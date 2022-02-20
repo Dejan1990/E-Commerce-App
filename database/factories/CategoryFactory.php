f956ec611f02f4a6530f7bd0acae022fe6425789
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-02-20 15:41:03 +0100

    Category part-1

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->words(rand(1, 2), true),
+            'description' => $this->faker->realText(rand(90, 100))
+        ];
+    }
+}
